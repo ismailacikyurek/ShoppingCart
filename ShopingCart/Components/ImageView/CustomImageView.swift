8347bcbcfb62d977ce374beff3681710c97caec7
ismailacikyurek <ismailackyurek@gmail.com>
2023-03-24 22:29:56 +0300

    extensions UIImageView

--- ShopingCart/Components/ImageView/CustomImageView.swift
+++ ShopingCart/Components/ImageView/CustomImageView.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 extension UIImageView {
-    func createUIImageView(image: UIImage? = nil,tintColor: UIColor? = nil,backgroundColor: UIColor? = nil, contentMode: ContentMode, maskedToBounds: Bool? = nil, cornerRadius: CGFloat? = nil, isUserInteractionEnabled: Bool? = nil,borderColor : CGColor? = nil,borderWidth : CGFloat? = nil,zPosition : CGFloat? = nil ) {
+    func createUIImageView(image: UIImage? = nil,tintColor: UIColor? = nil,backgroundColor: UIColor? = nil, contentMode: ContentMode? = nil, maskedToBounds: Bool? = nil, cornerRadius: CGFloat? = nil, isUserInteractionEnabled: Bool? = nil,borderColor : CGColor? = nil,borderWidth : CGFloat? = nil,zPosition : CGFloat? = nil ) {
         self.layer.masksToBounds = maskedToBounds ?? false
         self.image = image
         self.layer.cornerRadius = cornerRadius ?? 0
         self.isUserInteractionEnabled = isUserInteractionEnabled ?? true
-        self.contentMode = contentMode
+        self.contentMode = contentMode ?? .scaleToFill
         self.tintColor = tintColor
         self.backgroundColor = backgroundColor
         self.layer.borderColor = borderColor
